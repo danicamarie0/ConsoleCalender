ed7dd712232ebf962c61c7275d06bb5f3f47b549
danicamarie0 <danica0324@gmail.com>
2025-09-01 16:51:44 +0100

    format fix

--- ConsoleCalender/Calender.cs
+++ ConsoleCalender/Calender.cs
@@ -1,35 +1,37 @@
-﻿    namespace ConsoleCalender
+﻿namespace ConsoleCalender
+{
+    public class Calendar
     {
-        public class Calendar
-        {
         private List<NoteEntry> entries = CSVData.GetAll();
-            public void AddEntry(DateOnly date, string note)
-            {
-                entries.Add(new NoteEntry(date, note));
-                CSVData.Overwrite(entries);
+        public void AddEntry(DateOnly date, string note)
+        {
+            entries.Add(new NoteEntry(date, note));
+            CSVData.Overwrite(entries);
         }
-            public bool EditEntry(int index, string newNote)
-            {
-                if (index < 0 || index >= entries.Count) return false;
 
-                entries[index].Note = newNote;
-                CSVData.Overwrite(entries);
-                return true;
-            }
-            public bool DeleteEntry(int index)
-            {
-                if (index < 0 || index >= entries.Count) return false;
+        public bool EditEntry(int index, string newNote)
+        {
+            if (index < 0 || index >= entries.Count) return false;
 
-                entries.RemoveAt(index);
-                CSVData.Overwrite(entries);
-                return true;
-            }
-
-            public List<NoteEntry> GetAllEntries()
-            {
-                return entries;
-            }
-
-            public int Count => entries.Count;
+            entries[index].Note = newNote;
+            CSVData.Overwrite(entries);
+            return true;
         }
+
+        public bool DeleteEntry(int index)
+        {
+            if (index < 0 || index >= entries.Count) return false;
+
+            entries.RemoveAt(index);
+            CSVData.Overwrite(entries);
+            return true;
+        }
+
+        public List<NoteEntry> GetAllEntries()
+        {
+            return entries;
+        }
+
+        public int Count => entries.Count;
     }
+}
