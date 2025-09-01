991f1a78835f07cc74b945220a865d677a8fe794
danicamarie0 <danica0324@gmail.com>
2025-09-01 16:26:37 +0100

    remove unneccesarry comments

--- ConsoleCalender/CSVData.cs
+++ ConsoleCalender/CSVData.cs
@@ -20,33 +20,6 @@
             return entries;
         }
 
-        //private static void Add(DateOnly date, string note)
-        //{
-        //    var entries = GetAll();
-        //    entries.Add(new NoteEntry(date, note));
-        //    Overwrite(entries);
-        //}
-
-        //public static bool Edit(int index, string newNote)
-        //{
-        //    var entries = GetAll();
-        //    if (index < 0 || index >= entries.Count) return false;
-
-        //    entries[index] = new NoteEntry(entries[index].Date, newNote);
-        //    Overwrite(entries);
-        //    return true;
-        //}
-
-        //public static bool Delete(int index)
-        //{
-        //    var entries = GetAll();
-        //    if (index < 0 || index >= entries.Count) return false;
-
-        //    entries.RemoveAt(index);
-        //    Overwrite(entries);
-        //    return true;
-        //}
-
         public static void Overwrite(List<NoteEntry> entries)
         {
             var outLines = new List<string>
