d11e44559c2da2746230b18c5fb941ee4f8711e3
danicamarie0 <137822768+danicamarie0@users.noreply.github.com>
2025-09-01 08:54:43 -0700

    Save to CSV
    Save to CSV

---
+++ ConsoleCalender/CSVData.cs
@@ -0,0 +1,40 @@
+﻿namespace ConsoleCalender
+{
+    internal class CSVData
+    {
+        private static readonly string filePath = "note.csv";
+
+        public static List<NoteEntry> GetAll()
+        {
+            var entries = new List<NoteEntry>();
+            if (!File.Exists(filePath)) return entries;
+            //check if a file exist 
+            foreach (var line in File.ReadAllLines(filePath))
+            {
+                var parts = line.Split(',', 2); //method to break string
+                if (parts.Length == 2 && DateOnly.TryParse(parts[0], out var date))
+                {
+                    entries.Add(new NoteEntry(date, parts[1]));
+                }
+            }
+            return entries;
+        }
+
+        public static void Overwrite(List<NoteEntry> entries)
+        {
+            var outLines = new List<string>
+              {
+                "Date,Note"
+              };
+
+            foreach (var entry in entries)
+            {
+                outLines.Add($"{entry.Date:yyyy-MM-dd},{entry.Note}");
+            }
+
+            File.WriteAllLines(filePath, outLines);
+        }
+
+
+    }
+}
